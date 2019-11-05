33efb45a659579da524d7082dfff51bb75cf7482
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2019-11-05 23:55:17 +0700

    updates

--- scripts/web-app/deploy.sh
+++ scripts/web-app/deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail
-#. ./scripts/web-app/.environment
+. ./scripts/web-app/.environment
 WADIR=./src/web-app
 WSDIR=./src/web-srv
 IFILE=${WSDIR}/react-app/index.html
 OFILE=${WSDIR}/src/views/layout-for-react-app.hbs
 #
-sh ./scripts/web-app/yarn-build.sh && \
+sh ./scripts/web-app/yarn-release.sh && \
 rm -f -R ${WSDIR}/react-app && \
-cp -R ${WADIR}/build ${WSDIR}/react-app && \
-node ./scripts/web-srv/make-hbs-layout.js \
+cp -R ${WADIR}/build/react-app ${WSDIR}/react-app && \
+node scripts/web-srv/make-hbs-layout.js \
   --input-file=${IFILE} \
   --output-file=${OFILE} && \
 rm ${WSDIR}/react-app/index.html && exit 0
